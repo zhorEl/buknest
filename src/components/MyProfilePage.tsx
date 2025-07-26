70dd7a4538845c973df08782dc344cd4b75b82be
zhorEl <elzhor.natinga@gmail.com>
2025-07-26 19:18:38 +0800

    Updated MyProfilePage.tsx

--- src/components/MyProfilePage.tsx
+++ src/components/MyProfilePage.tsx
@@ -351,6 +351,9 @@ export default function MyProfilePage({ user, onPageChange }: MyProfilePageProps
           </div>
         </div>
       </div>
+      
+      {/* Verification Modal */}
+      <VerificationModal />
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         {/* Profile Header */}
